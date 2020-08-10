af2f8234748ce8981ca81dc4cd14bec3e79c381e
ebertm777 <eber.tmacedo@gmail.com>
2020-08-10 18:42:04 -0300

    added a heroku backend for pimp

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
-const URL_BACKEND = window.location.hostname.includes('localhost')
-    ? 'http://localhost:8080' : 'https://koalaplaybeta.herokuapp.com/';
+const URL_BACKEND = 'https://koalaplaybeta.herokuapp.com'
 
 export default {
     URL_BACKEND,
