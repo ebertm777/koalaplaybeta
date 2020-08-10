537d4dbaed7243022a83ecfdd887f8d7d8a63526
ebertm777 <eber.tmacedo@gmail.com>
2020-08-10 17:55:36 -0300

    backend changes

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
 });
