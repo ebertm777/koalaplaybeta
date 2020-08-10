d1052661b3c191ba0ec5938f621a50989ebdb589
ebertm777 <eber.tmacedo@gmail.com>
2020-08-10 18:08:22 -0300

    new back end features

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
-    ? 'http://localhost:8080' : '';
+    ? 'http://localhost:8080' : 'https://koalaplaybeta.herokuapp.com/';
 
 export default {
     URL_BACKEND,
