d5d3e1748c0e915a89983e3877ffbfa063110004
ebertm777 <eber.tmacedo@gmail.com>
2020-08-10 21:08:23 -0300

    category form updates

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -87,7 +87,7 @@ const CadastroCategoria = () => {
 
   useEffect(() => {
     const url = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080/categorias' : 'https://hyperflix.herokuapp.com/categorias';
+      ? 'http://localhost:8080/categorias' : 'https://koalaplaybeta.herokuapp.com/categorias';
     fetch(url).then(async (res) => {
       const resposta = await res.json();
       setCategorias([
@@ -113,15 +113,15 @@ const CadastroCategoria = () => {
           clearForm(valoresIniciais);
         }}
         >
-          <FormField tag="input" label="Nome da Categoria" type="text" name="nome" value={values.nome} onChange={handleChange}/>
-          <FormField tag="textarea" label="Descrição" type="text" name="descricao" value={values.descricao} onChange={handleChange}/>
+          <FormField tag="input" label="Nome da Categoria" type="text" name="nome" value={values.nome} onChange={handleChange} />
+          <FormField tag="textarea" label="Descrição" type="text" name="descricao" value={values.descricao} onChange={handleChange} />
           <FormField tag="input" label="Cor" type="color" name="cor" value={values.cor} onChange={handleChange} />
           <Button>
             Cadastrar
           </Button>
         </form>
-          {categorias.length === 0 && (
-        <Loading/>
+        {categorias.length === 0 && (
+          <Loading />
         )}
         <CategoryList>
           {categorias.map((categoria) => (
@@ -133,8 +133,8 @@ const CadastroCategoria = () => {
                 {categoria.descricao}
               </p>
               <Trash className="svg-icon" viewBox="0 0 20 20">
-							  <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
-						  </Trash>
+                <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
+              </Trash>
             </li>
           ))}
         </CategoryList>
